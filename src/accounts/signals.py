21b434ad69746119b84e1ea6143fa3db0a50338c
Alex Urassa <alexurassa03@gmail.com>
2023-06-06 07:08:12 +0300

    accounts > Add User `id` generator signal.

---
+++ src/accounts/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+
+from shared.utils import UUIDGenerator
+
+User = get_user_model()
+
+
+@receiver(pre_save, sender=User)
+def generateUserId(sender, instance, **kwargs):
+    if instance.id is None or instance.id == '':
+        instance.id = UUIDGenerator.generate_string_token(length=20)
+        instance.save()
