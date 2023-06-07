8b82332ba5ddbb9c84494dba20b27eca710fec20
Alex Urassa <alexurassa03@gmail.com>
2023-06-08 00:32:36 +0300

    shared > Add ability to add dashes to the generated token

--- src/shared/utils/uuid_generator.py
+++ src/shared/utils/uuid_generator.py
@@ -5,12 +5,17 @@ class UUIDGenerator:
     """Generates UUID Tokens"""
 
     @classmethod
-    def generate_string_token(cls, length: int = 16, is_upper: bool = False) -> str:
+    def generate_string_token(
+        cls, length: int = 16, is_upper: bool = False, has_dashes: bool = False
+    ) -> str:
         """Generates a string based token with no spaces default to \'16 chars\' long
         and \'lowercase\'
         """
+        if has_dashes:
+            token = str(uuid.uuid1())[:length]
+        else:
+            token = str(uuid.uuid4()).replace("-", "")[:length]
 
-        token = str(uuid.uuid1()).replace("-", "")[:length]
         if is_upper:
             return token.upper()
         return token
